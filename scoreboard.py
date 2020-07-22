53122be5f2804c05b87d41701e571facfc1529f1
PureNothingness <fg2020husky@gmail.com>
2020-07-22 16:29:11 -0700

    Done! 7/22/20

--- scoreboard.py
+++ scoreboard.py
@@ -66,7 +66,7 @@ class Scoreboard:
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
             ship = Ship(self.ai_game)
-            ship.rect.x = 10 + ship_number * ship_rect.width
+            ship.rect.x = 10 + ship_number * ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
 
